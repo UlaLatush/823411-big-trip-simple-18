3776494b7a8a746f7dcaffe33b43e9c0c2637381
UlaLatush <ula2sh@gmail.com>
2022-09-02 17:42:09 +0400

    add empty list message

--- src/mock/const.js
+++ src/mock/const.js
@@ -1,5 +1,5 @@
 const POINT_TYPES = ['taxi','bus','train','ship','drive','flight','check-in', 'sightseeing','restaurant'];
 
-const POINT_COUNT = 0;
+const POINT_COUNT = 4;
 
 export {POINT_TYPES, POINT_COUNT};
